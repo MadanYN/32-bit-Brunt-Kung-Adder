384c3521e8be607f54a630828bd74ad709afe523
MadanYN <madanyn3@gmail.com>
2023-11-02 12:40:10 +0530

    adder corrected

--- main/adder.vhd
+++ main/adder.vhd
@@ -34,64 +34,64 @@ begin
     end generate g0_p0_generate;
 
     g1_p1_generate: for i in 0 to 15 generate
-        gen1 : entity work.G_and_P port map (G_u => G0(i+1), P_u => P0(i+1), G_l => G0(i), P_l => P0(i), Gout => G1(i), Pout => P1(i));
+        gen1 : entity work.G_and_P port map (G_u => G0(2*i+1), P_u => P0(2*i+1), G_l => G0(2*i), P_l => P0(2*i), Gout => G1(i), Pout => P1(i));
     end generate g1_p1_generate;
 
     g2_p2_generate: for i in 0 to 7 generate
-        gen2 : entity work.G_and_P port map (G_u => G1(i+1), P_u => P1(i+1), G_l => G1(i), P_l => P1(i), Gout => G1(i), Pout => P1(i));
+        gen2 : entity work.G_and_P port map (G_u => G1(2*i+1), P_u => P1(2*i+1), G_l => G1(2*i), P_l => P1(2*i), Gout => G2(i), Pout => P2(i));
     end generate g2_p2_generate;
 
     g3_p3_generate: for i in 0 to 3 generate
-        gen3 : entity work.G_and_P port map (G_u => G2(i+1), P_u => P2(i+1), G_l => G2(i), P_l => P2(i), Gout => G3(i), Pout => P3(i));
+        gen3 : entity work.G_and_P port map (G_u => G2(2*i+1), P_u => P2(2*i+1), G_l => G2(2*i), P_l => P2(2*i), Gout => G3(i), Pout => P3(i));
     end generate g3_p3_generate;
 
     g4_p4_generate: for i in 0 to 1 generate
-        gen4 : entity work.G_and_P port map (G_u => G3(i+1), P_u => P3(i+1), G_l => G3(i), P_l => P3(i), Gout => G4(i), Pout => P4(i));
+        gen4 : entity work.G_and_P port map (G_u => G3(2*i+1), P_u => P3(2*i+1), G_l => G3(2*i), P_l => P3(2*i), Gout => G4(i), Pout => P4(i));
     end generate g4_p4_generate;
 
     gen5 : entity work.G_and_P port map (G_u => G4(1), P_u => P4(1), G_l => G4(0), P_l => P4(0), Gout => G5, Pout => P5);
 
     --Computation of carry
-    carry1 : entity work.Cin_map_G port map (A => G0(0), B => P0(0), Cin => Cin, Bit0_G => carry(1));
+    carry1 : entity work.abcgate port map (A => G0(0), B => P0(0), C => Cin, abc => carry(1));
 
-    carry2 : entity work.Cin_map_G port map (A => G1(0), B => P1(0), Cin => Cin, Bit0_G => carry(2));
+    carry2 : entity work.abcgate port map (A => G1(0), B => P1(0), C => Cin, abc => carry(2));
 
-    carry3 : entity work.Cin_map_G port map (A => G0(2), B => P0(2), Cin => carry(2), Bit0_G => carry(3));
-    carry4 : entity work.Cin_map_G port map (A => G2(0), B => P2(0), Cin => Cin, Bit0_G => carry(4));
+    carry3 : entity work.abcgate port map (A => G0(2), B => P0(2), C => carry(2), abc => carry(3));
+    carry4 : entity work.abcgate port map (A => G2(0), B => P2(0), C => Cin, abc => carry(4));
 
-    carry5 : entity work.Cin_map_G port map (A => G0(4), B => P0(4), Cin => carry(4), Bit0_G => carry(5));
-    carry6 : entity work.Cin_map_G port map (A => G1(2), B => P1(2), Cin => carry(4), Bit0_G => carry(6));
-    carry8 : entity work.Cin_map_G port map (A => G3(0), B => P3(0), Cin => Cin, Bit0_G => carry(8));
+    carry5 : entity work.abcgate port map (A => G0(4), B => P0(4), C => carry(4), abc => carry(5));
+    carry6 : entity work.abcgate port map (A => G1(2), B => P1(2), C => carry(4), abc => carry(6));
+    carry8 : entity work.abcgate port map (A => G3(0), B => P3(0), C => Cin, abc => carry(8));
 
-    carry7 : entity work.Cin_map_G port map (A => G0(6), B => P0(6), Cin => carry(6), Bit0_G => carry(7));
-    carry9 : entity work.Cin_map_G port map (A => G0(8), B => P0(8), Cin => carry(8), Bit0_G => carry(9));
-    carry10 : entity work.Cin_map_G port map (A => G1(4), B => P1(4), Cin => carry(8), Bit0_G => carry(10));
-    carry12 : entity work.Cin_map_G port map (A => G2(2), B => P2(2), Cin => carry(8), Bit0_G => carry(12));
-    carry16 : entity work.Cin_map_G port map (A => G4(0), B => P4(0), Cin => Cin, Bit0_G => carry(16));
+    carry7 : entity work.abcgate port map (A => G0(6), B => P0(6), C => carry(6), abc => carry(7));
+    carry9 : entity work.abcgate port map (A => G0(8), B => P0(8), C => carry(8), abc => carry(9));
+    carry10 : entity work.abcgate port map (A => G1(4), B => P1(4), C => carry(8), abc => carry(10));
+    carry12 : entity work.abcgate port map (A => G2(2), B => P2(2), C => carry(8), abc => carry(12));
+    carry16 : entity work.abcgate port map (A => G4(0), B => P4(0), C => Cin, abc => carry(16));
 
-    carry11 : entity work.Cin_map_G port map (A => G0(10), B => P0(10), Cin => carry(10), Bit0_G => carry(11));
-    carry13 : entity work.Cin_map_G port map (A => G0(12), B => P0(12), Cin => carry(12), Bit0_G => carry(13));
-    carry14 : entity work.Cin_map_G port map (A => G1(6), B => P1(6), Cin => carry(12), Bit0_G => carry(14));
-    carry17 : entity work.Cin_map_G port map (A => G0(16), B => P0(16), Cin => carry(16), Bit0_G => carry(17));
-    carry18 : entity work.Cin_map_G port map (A => G1(8), B => P1(8), Cin => carry(16), Bit0_G => carry(18));
-    carry20 : entity work.Cin_map_G port map (A => G2(4), B => P2(4), Cin => carry(16), Bit0_G => carry(20));
-    carry24 : entity work.Cin_map_G port map (A => G3(2), B => P3(2), Cin => carry(16), Bit0_G => carry(16));
-    carry32 : entity work.Cin_map_G port map (A => G5, B => P5, Cin => Cin, Bit0_G => Cout);
+    carry11 : entity work.abcgate port map (A => G0(10), B => P0(10), C => carry(10), abc => carry(11));
+    carry13 : entity work.abcgate port map (A => G0(12), B => P0(12), C => carry(12), abc => carry(13));
+    carry14 : entity work.abcgate port map (A => G1(6), B => P1(6), C => carry(12), abc => carry(14));
+    carry17 : entity work.abcgate port map (A => G0(16), B => P0(16), C => carry(16), abc => carry(17));
+    carry18 : entity work.abcgate port map (A => G1(8), B => P1(8), C => carry(16), abc => carry(18));
+    carry20 : entity work.abcgate port map (A => G2(4), B => P2(4), C => carry(16), abc => carry(20));
+    carry24 : entity work.abcgate port map (A => G3(2), B => P3(2), C => carry(16), abc => carry(24));
+    carry32 : entity work.abcgate port map (A => G5, B => P5, C => Cin, abc => Cout);
 
-    carry15 : entity work.Cin_map_G port map (A => G1(14), B => P1(14), Cin => carry(14), Bit0_G => carry(15));
-    carry19 : entity work.Cin_map_G port map (A => G0(18), B => P0(18), Cin => carry(18), Bit0_G => carry(19));
-    carry21 : entity work.Cin_map_G port map (A => G0(20), B => P0(20), Cin => carry(20), Bit0_G => carry(21));
-    carry22 : entity work.Cin_map_G port map (A => G1(10), B => P1(10), Cin => carry(20), Bit0_G => carry(22));
-    carry26 : entity work.Cin_map_G port map (A => G1(12), B => P1(12), Cin => carry(24), Bit0_G => carry(26));
-    carry25 : entity work.Cin_map_G port map (A => G0(24), B => P0(24), Cin => carry(24), Bit0_G => carry(25));
-    carry28 : entity work.Cin_map_G port map (A => G2(6), B => P2(6), Cin => carry(24), Bit0_G => carry(28));
+    carry15 : entity work.abcgate port map (A => G0(14), B => P0(14), C => carry(14), abc => carry(15));
+    carry19 : entity work.abcgate port map (A => G0(18), B => P0(18), C => carry(18), abc => carry(19));
+    carry21 : entity work.abcgate port map (A => G0(20), B => P0(20), C => carry(20), abc => carry(21));
+    carry22 : entity work.abcgate port map (A => G1(10), B => P1(10), C => carry(20), abc => carry(22));
+    carry26 : entity work.abcgate port map (A => G1(12), B => P1(12), C => carry(24), abc => carry(26));
+    carry25 : entity work.abcgate port map (A => G0(24), B => P0(24), C => carry(24), abc => carry(25));
+    carry28 : entity work.abcgate port map (A => G2(6), B => P2(6), C => carry(24), abc => carry(28));
 
-    carry23 : entity work.Cin_map_G port map (A => G0(22), B => P0(22), Cin => carry(22), Bit0_G => carry(23));
-    carry27 : entity work.Cin_map_G port map (A => G0(26), B => P0(26), Cin => carry(26), Bit0_G => carry(27));
-    carry29 : entity work.Cin_map_G port map (A => G0(28), B => P0(28), Cin => carry(28), Bit0_G => carry(29));
-    carry30 : entity work.Cin_map_G port map (A => G1(14), B => P1(14), Cin => carry(28), Bit0_G => carry(30));
+    carry23 : entity work.abcgate port map (A => G0(22), B => P0(22), C => carry(22), abc => carry(23));
+    carry27 : entity work.abcgate port map (A => G0(26), B => P0(26), C => carry(26), abc => carry(27));
+    carry29 : entity work.abcgate port map (A => G0(28), B => P0(28), C => carry(28), abc => carry(29));
+    carry30 : entity work.abcgate port map (A => G1(14), B => P1(14), C => carry(28), abc => carry(30));
     
-    carry31 : entity work.Cin_map_G port map (A => G0(30), B => P0(30), Cin => carry(30), Bit0_G => carry(31));
+    carry31 : entity work.abcgate port map (A => G0(30), B => P0(30), C => carry(30), abc => carry(31));
 
 
     --Computing sum
--- main/g_and_p.vhd
+++ main/g_and_p.vhd
@@ -11,6 +11,6 @@ end entity G_and_P;
 
 architecture arc of G_and_P is
     begin
-        gate1 : entity work.Cin_map_G port map (G_u, P_u, G_l, Gout);
+        gate1 : entity work.abcgate port map (G_u, P_u, G_l, Gout);
         gate2 : entity work.andgate port map (P_u, P_l, Pout);
 end arc;
